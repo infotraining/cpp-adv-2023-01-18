d38610eb5b206ac79bd6758ce04c59f58e3d962f
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-01-19 13:25:01 +0000

    Templates - 1

--- _exercises/templates-ex/tests_stack.cpp
+++ _exercises/templates-ex/tests_stack.cpp
@@ -1,167 +1,219 @@
-#include <catch2/catch_test_macros.hpp>
 #include <algorithm>
 #include <array>
+#include <catch2/catch_test_macros.hpp>
+#include <deque>
 #include <memory>
 
-// TEST_CASE("After construction", "[stack,constructors]")
-// {
-//     Stack<int> s;
+template <typename T, template <typename, typename> class Container = std::deque, typename TAllocator = std::allocator<T>>
+class Stack
+{
+    Container<T, TAllocator> m_buffer;
 
-//     SECTION("is empty")
-//     {
-//         REQUIRE(s.empty());
-//     }
+public:
+    bool empty() const { return m_buffer.empty(); }
 
-//     SECTION("size is zero")
-//     {
-//         REQUIRE(s.size() == 0);
-//     }
-// }
+    std::size_t size() const { return m_buffer.size(); }
 
-// TEST_CASE("Pushing an item", "[stack,push]")
-// {
-//     Stack<int> s;
+    // void push(const T& item)
+    // {
+    //     m_buffer.push_back(item);
+    // }
 
-//     SECTION("is no longer empty")
-//     {
-//         s.push(1);
+    // void push(T&& item)
+    // {
+    //     m_buffer.push_back(std::move(item));
+    // }
 
-//         REQUIRE(!s.empty());
-//     }
+    template <typename TItem>
+    void push(TItem&& item)
+    {
+        m_buffer.push_back(std::forward<TItem>(item));
+    }
 
-//     SECTION("size is increased")
-//     {
-//         auto size_before = s.size();
+    template <typename... TArgs>
+    void emplace(TArgs&&... args)
+    {
+        m_buffer.emplace_back(std::forward<TArgs>(args)...);
+    }
 
-//         s.push(1);
+    T& top()
+    {
+        return m_buffer.back();
+    }
 
-//         REQUIRE(s.size() - size_before == 1);
-//     }
+    void pop()
+    {
+        m_buffer.pop_back();
+    }
+};
 
-//     SECTION("recently pushed item is on a top")
-//     {
-//         s.push(4);
+TEST_CASE("After construction", "[stack,constructors]")
+{
+    Stack<int, std::vector> s;
 
-//         REQUIRE(s.top() == 4);
-//     }
-// }
+    SECTION("is empty")
+    {
+        REQUIRE(s.empty());
+    }
 
-// template <typename T>
-// std::vector<T> pop_all(Stack<T>& s)
-// {
-//     std::vector<T> values(s.size());
+    SECTION("size is zero")
+    {
+        REQUIRE(s.size() == 0);
+    }
+}
 
-//     for (auto& item : values)
-//     {
-//         item = std::move(s.top());
-//         s.pop();
-//     }
+SCENARIO("Stack - push")
+{
+    GIVEN("An empty stack")
+    {
+        Stack<int> s; // Arrange
 
-//     return values;
-// }
+        WHEN("item pushed to stack")
+        {
+            auto size_before = s.size();
+            s.push(1); // Act
 
-// TEST_CASE("Popping an item", "[stack,pop]")
-// {
-//     Stack<int> s;
+            THEN("is no longer empty")
+            {
+                REQUIRE(!s.empty()); // Assert
+            }
 
-//     s.push(1);
-//     s.push(4);
+            THEN("size is increased")
+            {
+                REQUIRE(s.size() - size_before == 1);
+            }
 
-//     int item;
+            THEN("recently pushed item is on a top")
+            {
+                REQUIRE(s.top() == 1);
+            }
+        }
+    }
+}
 
-//     SECTION("assignes an item from a top to an argument passed by ref")
-//     {
-//         item = s.top();
-//         s.pop();
+TEST_CASE("emplace")
+{
+    Stack<std::string> s;
 
-//         REQUIRE(item == 4);
-//     }
+    s.emplace(5, 'a');
 
-//     SECTION("size is decreased")
-//     {
-//         size_t size_before = s.size();
+    REQUIRE(s.top() == "aaaaa");
+}
 
-//         item = s.top();
-//         s.pop();
+template <typename T>
+std::vector<T> pop_all(Stack<T>& s)
+{
+    std::vector<T> values(s.size());
 
+    for (auto& item : values)
+    {
+        item = std::move(s.top());
+        s.pop();
+    }
 
-//         REQUIRE(size_before - s.size() == 1);
-//     }
+    return values;
+}
 
-//     SECTION("LIFO order")
-//     {
-//         int a, b;
+TEST_CASE("Popping an item", "[stack,pop]")
+{
+    Stack<int> s;
 
-//         a = s.top();
-//         s.pop();
+    s.push(1);
+    s.push(4);
 
-//         b = s.top();
-//         s.pop();
+    int item;
 
+    SECTION("assigne an item from a top to an argument passed by ref")
+    {
+        item = s.top();
+        s.pop();
 
-//         REQUIRE(a == 4);
-//         REQUIRE(b == 1);
-//     }
-// }
+        REQUIRE(item == 4);
+    }
 
-// TEST_CASE("Move semantics", "[stack,push,pop,move]")
-// {
-//     using namespace std::literals;
+    SECTION("size is decreased")
+    {
+        size_t size_before = s.size();
 
-//     SECTION("stores move-only objects")
-//     {
-//         auto txt1 = std::make_unique<std::string>("test1");
+        item = s.top();
+        s.pop();
 
-//         Stack<std::unique_ptr<std::string>> s;
+        REQUIRE(size_before - s.size() == 1);
+    }
 
-//         s.push(move(txt1));
-//         s.push(std::make_unique<std::string>("test2"));
+    SECTION("LIFO order")
+    {
+        int a, b;
 
-//         std::unique_ptr<std::string> value;
+        a = s.top();
+        s.pop();
 
-//         value = std::move(s.top());
-//         s.pop();
-//         REQUIRE(*value == "test2"s);
+        b = s.top();
+        s.pop();
 
-//         value = std::move(s.top());
-//         s.pop();
-//         REQUIRE(*value == "test1"s);
-//     }
+        REQUIRE(a == 4);
+        REQUIRE(b == 1);
+    }
+}
 
-//     SECTION("move constructor", "[stack,move]")
-//     {
-//         Stack<std::unique_ptr<std::string>> s;
+TEST_CASE("Move semantics", "[stack,push,pop,move]")
+{
+    using namespace std::literals;
 
-//         s.push(std::make_unique<std::string>("txt1"));
-//         s.push(std::make_unique<std::string>("txt2"));
-//         s.push(std::make_unique<std::string>("txt3"));
+    SECTION("stores move-only objects")
+    {
+        auto txt1 = std::make_unique<std::string>("test1");
 
-//         auto moved_s = std::move(s);
+        Stack<std::unique_ptr<std::string>> s;
 
-//         auto values = pop_all(moved_s);
+        s.push(move(txt1));
+        s.push(std::make_unique<std::string>("test2"));
 
-//         auto expected = {"txt3", "txt2", "txt1"};
-//         REQUIRE(std::equal(values.begin(), values.end(), expected.begin(), [](const auto& a, const auto& b) { return *a == b; }));
-//     }
+        std::unique_ptr<std::string> value;
 
-//     SECTION("move assignment", "[stack,move]")
-//     {
-//         Stack<std::unique_ptr<std::string>> s;
+        value = std::move(s.top());
+        s.pop();
+        REQUIRE(*value == "test2"s);
 
-//         s.push(std::make_unique<std::string>("txt1"));
-//         s.push(std::make_unique<std::string>("txt2"));
-//         s.push(std::make_unique<std::string>("txt3"));
+        value = std::move(s.top());
+        s.pop();
+        REQUIRE(*value == "test1"s);
+    }
 
-//         Stack<std::unique_ptr<std::string>> target;
-//         target.push(std::make_unique<std::string>("x"));
+    SECTION("move constructor", "[stack,move]")
+    {
+        Stack<std::unique_ptr<std::string>> s;
 
-//         target = std::move(s);
+        s.push(std::make_unique<std::string>("txt1"));
+        s.push(std::make_unique<std::string>("txt2"));
+        s.push(std::make_unique<std::string>("txt3"));
 
-//         REQUIRE(target.size() == 3);
+        auto moved_s = std::move(s);
 
-//         auto values = pop_all(target);
+        auto values = pop_all(moved_s);
 
-//         auto expected = {"txt3", "txt2", "txt1"};
-//         REQUIRE(std::equal(values.begin(), values.end(), expected.begin(), [](const auto& a, const auto& b) { return *a == b; }));
-//     }
-// }
+        auto expected = {"txt3", "txt2", "txt1"};
+        REQUIRE(std::equal(values.begin(), values.end(), expected.begin(), [](const auto& a, const auto& b) { return *a == b; }));
+    }
+
+    SECTION("move assignment", "[stack,move]")
+    {
+        Stack<std::unique_ptr<std::string>> s;
+
+        s.push(std::make_unique<std::string>("txt1"));
+        s.push(std::make_unique<std::string>("txt2"));
+        s.push(std::make_unique<std::string>("txt3"));
+
+        Stack<std::unique_ptr<std::string>> target;
+        target.push(std::make_unique<std::string>("x"));
+
+        target = std::move(s);
+
+        REQUIRE(target.size() == 3);
+
+        auto values = pop_all(target);
+
+        auto expected = {"txt3", "txt2", "txt1"};
+        REQUIRE(std::equal(values.begin(), values.end(), expected.begin(), [](const auto& a, const auto& b) { return *a == b; }));
+    }
+}
--- templates/tests_templates.cpp
+++ templates/tests_templates.cpp
@@ -5,17 +5,109 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <cstring>
+#include <array>
+#include <optional>
 
 using Utils::Gadget;
+using namespace std::literals;
+
+template <typename T> 
+T maximum(T a, T b)
+{
+    return a < b ? b : a;
+}
+
+const char* maximum(const char* a, const char* b)
+{
+    return std::strcmp(a, b) < 0 ? b : a; 
+}
+
+namespace Alt
+{
+    template <typename T1, typename T2>
+    std::common_type_t<T1, T2> maximum(T1 a, T2 b)
+    {
+        return a < b ? b : a;
+    }
+}
+
+template <typename InpIter, typename OutIter, typename Predicate>
+OutIter my_copy_if(InpIter first, InpIter last, OutIter dest, Predicate pred)
+{
+    for(InpIter it = first; it != last; ++it)
+    {
+        if (pred(*it))
+        {
+            *dest = *it;
+            ++dest;
+        }
+    }
+
+    return dest;
+}
 
 TEST_CASE("function templates")
 {
-    // TODO
+    std::cout << maximum(4, 8) << "\n";
+
+    std::cout << maximum("one"s, "two"s) << "\n";
+
+    std::cout << maximum("one", "two") << "\n";
+
+    auto result = Alt::maximum(3.14, 665);
+
+    std::array data = {1, 4, 6, 3, 7, 665, 42, 6};
+    std::vector<int> evens;
+
+    my_copy_if(data.begin(), data.end(), std::back_inserter(evens), [](int n) { return n % 2 == 0; });
+
+    REQUIRE(evens == std::vector{4, 6, 42, 6});
 }
+
+template <typename T1 = int, typename T2 = T1>
+struct ValuePair
+{
+    T1 first{};
+    T2 second{};
+
+    ValuePair() = default;
+
+    ValuePair(T1 fst, T2 snd) : first{fst}, second{snd}
+    {}
+
+    std::string to_string() const
+    {
+        std::stringstream ss;
+        ss << "[ " << first << ", " << second << " ]";
+
+        return ss.str();
+    }
+};
 
 TEST_CASE("class templates")
 {
-    // TODO
+    ValuePair<int, double> v1{42, 3.14};
+    std::cout << v1.to_string() << "\n";
+
+    ValuePair<> v2{42, 665};
+    std::cout << v2.to_string() << "\n";
+
+    SECTION("Since C++17 - CTAD")
+    {
+        ValuePair vp1{42, 3.14}; // ValuePair<int, double>
+
+        ValuePair vp2{42, 42};  // ValuePair<int, int>
+
+        ValuePair vp3; // ValuePair<int, int>
+        std::cout << vp3.to_string() << "\n";
+
+        std::vector vec = {1, 2, 3, 4};
+        std::array numbers = {1, 2, 3, 4, 5};
+        std::optional opt = 42;
+        std::unique_ptr<int> ptr{new int(13)};
+        std::shared_ptr sptr = std::move(ptr);
+    }
 }
 
 TEST_CASE("template aliases")
